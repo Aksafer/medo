de5fec03ca73148e1e5fd907639018f97cb5a732
Team Lotus <104599580+Tm-Lotus@users.noreply.github.com>
2024-04-05 17:24:34 +0200

    Update play.py

--- MatrixMusic/utils/inline/play.py
+++ MatrixMusic/utils/inline/play.py
@@ -67,7 +67,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             
         ],[
             InlineKeyboardButton(text="᥉᥆ᥙᖇᥴᥱ ", url=f"https://t.me/SOURCE_SOM3A"),
-            InlineKeyboardButton(text="ᘜᖇ᥆ᥙρ", url=f"https://t.me/Q1_QU"),
+            InlineKeyboardButton(text="ᘜᖇ᥆ᥙρ", url=f"https://t.me/YR_HC"),
         ],[
             InlineKeyboardButton(text="Ꭵ᥉ ρᎥძᥱᖇ", url=f"https://t.me/Y_D_ll"),
         ],[
