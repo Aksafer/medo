2e767508172b7c0c7ea7abfdee7ca42e01e63d58
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-04-08 21:51:56 +0200

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Tm-Lotus/medo.git",
+    "https://github.com/Aksafer/medo",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
