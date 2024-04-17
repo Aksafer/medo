b80325da778299a7c0418621a7efd9b383658bb6
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-04-17 09:53:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -99,16 +99,16 @@ START_IMG_URL = getenv(
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://graph.org/file/3f428a46f14944f328804.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/bfbc1545062baa6710301.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/3f428a46f14944f328804.jpg"
 STATS_IMG_URL = "https://graph.org/file/3f428a46f14944f328804.jpg"
 TELEGRAM_AUDIO_URL = "https://graph.org/file/3f428a46f14944f328804.jpg"
 TELEGRAM_VIDEO_URL = "https://graph.org/file/3f428a46f14944f328804.jpg"
 STREAM_IMG_URL ="https://graph.org/file/3f428a46f14944f328804.jpg"
 SOUNCLOUD_IMG_URL = "https://graph.org/file/3f428a46f14944f328804.jpg"
-YOUTUBE_IMG_URL = "https://graph.org/file/3f428a46f14944f328804.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/bfbc1545062baa6710301.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/3f428a46f14944f328804.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/3f428a46f14944f328804.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/3f428a46f14944f328804.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/bfbc1545062baa6710301.jpg"
 
 
 def time_to_seconds(time):
