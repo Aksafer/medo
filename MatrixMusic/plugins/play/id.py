4b18677df91e6e68cca08f375b6999ff508c7298
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-04-19 03:34:48 +0200

    Add files via upload

--- MatrixMusic/plugins/play/id.py
+++ MatrixMusic/plugins/play/id.py
@@ -6,44 +6,45 @@ from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
+
 iddof = []
 @app.on_message(
     command(["قفل الايدي","تعطيل الايدي"])
     & filters.group
-  
+    & ~filters.edited
 )
 async def iddlock(client, message):
    get = await app.get_chat_member(message.chat.id, message.from_user.id)
    if get.status in ["creator", "administrator"]:
       if message.chat.id in iddof:
-        return await message.reply_text("تم معطل من قبل🔒")
+        return await message.reply_text("تم معطل من قبل \n√")
       iddof.append(message.chat.id)
-      return await message.reply_text("تم تعطيل الايدي بنجاح ✅🔒")
+      return await message.reply_text("تم تعطيل الايدي بنجاح √")
    else:
-      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
+      return await message.reply_text("لازم تكون ادمن \n√")
 
 @app.on_message(
     command(["فتح الايدي","تفعيل الايدي"])
     & filters.group
-  
+    & ~filters.edited
 )
 async def iddopen(client, message):
    get = await app.get_chat_member(message.chat.id, message.from_user.id)
    if get.status in ["creator", "administrator"]:
       if not message.chat.id in iddof:
-        return await message.reply_text("الايدي مفعل من قبل ✅")
+        return await message.reply_text("الايدي مفعل من قبل √")
       iddof.remove(message.chat.id)
-      return await message.reply_text("تم فتح الايدي بنجاح ✅🔓")
+      return await message.reply_text("تم فتح الايدي بنجاح √")
    else:
-      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
+      return await message.reply_text("لازم تكون ادمن \n√")
 
 
 
 
 @app.on_message(
-    command(["ايدي","الايدي","ا"])
+    command(["ايدي","id","ا"])
     & filters.group
-  
+    & ~filters.edited
 )
 async def iddd(client, message):
     if message.chat.id in iddof:
@@ -51,7 +52,7 @@ async def iddd(client, message):
     usr = await client.get_chat(message.from_user.id)
     name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"""🤡 ¦𝙽𝙰𝙼𝙴 :{message.from_user.mention}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{message.from_user.username}\n🎃 ¦𝙸𝙳 :`{message.from_user.id}`\n💌 ¦𝙱𝙸𝙾 :{usr.bio}\n✨ ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n♻️ ¦𝙸𝙳.𝙲𝙷𝙰𝚃 :`{message.chat.id}`""", 
+    await message.reply_photo(photo,       caption=f""" - ꪀᥲ️︎ꪔᥱ︎ :{message.from_user.mention}\n- u᥉ᥱ︎ɾ :@{message.from_user.username}\n- Ꭵძ . :`{message.from_user.id}`\nႦᎥ᥆ :{usr.bio}\nᥴ𝗁ᥲ️ƚ: {message.chat.title}\n𝚒𝚍 𝚐𝚛𝚘𝚞𝚋 :`{message.chat.id}`""", 
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -68,40 +69,63 @@ iddof = []
 @app.on_message(
     command(["قفل جمالي","تعطيل جمالي"])
     & filters.group
-  
+    & ~filters.edited
 )
 async def lllock(client, message):
    get = await app.get_chat_member(message.chat.id, message.from_user.id)
    if get.status in ["creator", "administrator"]:
       if message.chat.id in iddof:
-        return await message.reply_text("جمالي معطل من قبل✅")
+        return await message.reply_text("جمالي معطل من قبل√")
       iddof.append(message.chat.id)
-      return await message.reply_text(" تم تعطيل جمالي بنجاح✅🔒")
+      return await message.reply_text(" تم تعطيل جمالي بنجاح√")
    else:
-      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
+      return await message.reply_text("لازم تكون ادمن\n√")
 
 @app.on_message(
     command(["فتح جمالي","تفعيل جمالي"])
     & filters.group
-  
+    & ~filters.edited
 )
 async def idljjopen(client, message):
    get = await app.get_chat_member(message.chat.id, message.from_user.id)
    if get.status in ["creator", "administrator"]:
       if not message.chat.id in iddof:
-        return await message.reply_text("جمالي مفعل من قبل✅")
+        return await message.reply_text("جمالي مفعل من قبل√")
       iddof.remove(message.chat.id)
-      return await message.reply_text("تم فتح جمالي بنجاح ✅🔓")
+      return await message.reply_text("تم فتح جمالي بنجاح √")
    else:
-      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
+      return await message.reply_text("هاذا الامر لأدمن فقط")
 
+
+
+@app.on_message(filters.command(['تفعيل التعديل'], prefixes=""))
+async def iddlock(client, message):
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if get.status in ["creator", "administrator"]:
+        if message.chat.id in italy:
+            return await message.reply_text("تم تفعيل التعديل \n√")
+        italy.append(message.chat.id)
+        return await message.reply_text("تم تفعيل التعديل بنجاح \n√")
+    else:
+        return await message.reply_text("يجب عليك أن تكون مشرف اولا \n√")
+
+@app.on_message(filters.command(['تعطيل التعديل'], prefixes=""))
+async def iddopen(client, message):
+   get = await app.get_chat_member(message.chat.id, message.from_user.id)
+   if get.status in ["creator", "administrator"]:
+      if not message.chat.id in italy:
+        return await message.reply_text("التعديل معطل من قبل \n√")
+      italy.remove(message.chat.id)
+      return await message.reply_text("تم فتح تعطيل بنجاح \n√")
+   else:
+      return await message.reply_text("لازم تكون ادمن اولا \n√")
 
 
 
 @app.on_message(
     command(["جمالي"])
     & filters.group
-  
+    & ~filters.edited
 )
 async def idjjdd(client, message):
     if message.chat.id in iddof:
@@ -111,7 +135,7 @@ async def idjjdd(client, message):
     i = ["0","10", "15","20", "25","30","35", "40","45", "50","55", "60"," 66", "70","77", "80","85", "90","99", "100","1000" ]
     ik = random.choice(i)
     photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"نسبه جمالك يا مز انت \n│ \n└ʙʏ: {ik} %😂❤️", 
+    await message.reply_photo(photo,       caption=f"نسبه جمالك يا مز انت \n※ \n🐉: {ik} %😂❤️", 
     reply_markup=InlineKeyboardMarkup(
             [
                 [
