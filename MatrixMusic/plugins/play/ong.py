80a39760b74c561fa48ef6e10df126c74c8998cc
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-04-26 22:10:47 +0300

    Update ong.py

--- MatrixMusic/plugins/play/ong.py
+++ MatrixMusic/plugins/play/ong.py
@@ -1,40 +1,43 @@
+from MatrixMusic import app
 import asyncio
 import random
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import filters, Client
-from MatrixMusic import app
 
-@app.on_message(filters.command(['نداء','ن'], ""))
-async def call_random_members(client: Client, message: Message):
+
+@app.on_message(filters.command(['نداء'], prefixes=""))
+def call_random_member(client, message):
     chat_id = message.chat.id
     members = [
-        member for member in client.get_chat_memberss(chat_id)
+        member for member in client.iter_chat_members(chat_id)
         if not member.user.is_bot
     ]
-    random_members = random.choice(members)
-    random_members_mention = f"[{random_members.user.first_name}](tg://user?id={random_members.user.id})"
+    random_member = random.choice(members)
+    random_member_mention = f"{random_member.user.first_name}"
     random_message = random.choice([
-        f"ووين ككارس لنا واجد نرجو فيك 😾 {random_members_mention}",
-        f"• يـا قمـري ❤️‍🔥 {random_members_mention}",
-        f"حبي فوتك من الخاص وتعال 🤔 {random_members_mention}",
-        f"• يـا راس السطل تعال {random_members_mention}",
-        f"• انت ليش قمر هكي 🌚♥ {random_members_mention}"
+        f"بقلنا ساعه مستنينك فينك 😾 {random_member_mention}",
+        f"• يـا قمـري ❤️‍🔥 {random_member_mention}",
+        f"حبيبي لي م بتتكلم معنا 🤔 {random_member_mention}",
+        f"• يـا تفاحه 🍏 فينك {random_member_mention}",
+        f"• هو انت لي قمر كده 🌚♥ {random_member_mention}"
     ])
     client.send_message(chat_id, random_message, reply_to_message_id=message.message_id, parse_mode='markdown')
 
 
 
-@app.on_message(filters.command(['زوجني','ز'], ""))
-async def call_random_members(client: Client, message: Message):
+@app.on_message(filters.command(['زوجني'], prefixes=""))
+def call_random_member(client, message):
     chat_id = message.chat.id
     members = [
-        member for member in client.get_chat_memberss(chat_id)
+        member for member in client.iter_chat_members(chat_id)
         if not member.user.is_bot
     ]
-    random_members = random.choice(members)
-    random_members_mention = f"[{random_members.user.first_name}](tg://user?id={random_members.user.id})"
+    random_member = random.choice(members)
+    random_member_mention = f"{random_member.user.first_name}"
     random_message = random.choice([
-        f"• اخترت لك هذا الشخص {random_members_mention} \n 🙈♥",
-        f"• اخترت لك هذا الشخص \n {random_members_mention} \n **"
+        f"• اخترت لك هذا الشخص {random_member_mention} \n اعلنكم الان امام الجروب زوجاً وزوجه يلا روحو بف اعملو واحد🙈♥",
+        f"• اخترت لك هذا الشخص \n {random_member_mention} \n انتم الان متزوجين رسميا يلا روحو بف اعملو واحد🌚♥"
     ])
     client.send_message(chat_id, random_message, reply_to_message_id=message.message_id, parse_mode='markdown')
+
+print("OKAY ITALY MUSIC CODE WORKING NOW⚡")
