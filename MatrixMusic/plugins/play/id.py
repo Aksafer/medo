199140253f29cf1867a5f00744ba78f2650713c6
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-04-26 19:28:36 +0300

    Update id.py

--- MatrixMusic/plugins/play/id.py
+++ MatrixMusic/plugins/play/id.py
@@ -9,7 +9,99 @@ from pyrogram.enums import ParseMode, ChatMemberStatus
 
 
 iddof = []
+id = {}
+mport asyncio
+from config import OWNER_ID
+from pyrogram import Client, filters
+from MatrixMusic import app
+import random
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.enums import ParseMode, ChatMemberStatus
 
+
+iddof = []
+id = {}
+
+@app.on_message(
+     command(["قفل ايدي","تعطيل ايدي"])
+     & filters.group
+
+   
+)
+async def iddlock(client:Client, message:Message):
+    dev = (OWNER_ID)
+    haya = (6456857472)
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if int(message.from_user.id) == haya:
+       rotba= "مّمٌَـبـ ـࢪمـج السوࢪس"
+    elif message.from_user.id in dev:
+        rotba = "مطور اساسي"
+    elif get.status in [ChatMemberStatus.OWNER]:
+        rotba= "المــــــألك"
+    elif get.status in [ChatMemberStatus.ADMINISTRATOR]:
+        rotba= "أدمــــــن"
+    else:   
+        return await message.reply_text(f"يا {message.from_user.mention} انت لست مشرفا هنا")    
+     
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR] and  dev:
+        if message.chat.id in iddof:
+            return await message.reply_text(f"يا {message.from_user.mention}\n الايدي مقفله من قبل")
+        iddof.append(message.chat.id)
+        return await message.reply_text(f"تم قفل الايدي بنجاح\n\n بواسطة {rotba} ←{message.from_user.mention}")
+    else:
+        return await message.reply_text(f"يا {message.from_user.mention} انت لست مشرفا هنا")
+
+@app.on_message(
+    command(["فتح ايدي","تفعيل ايدي"])
+    & filters.group
+)
+async def idljjopen(client, message):
+    dev = (OWNER_ID)
+    haya = (6456857472)
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if int(message.from_user.id) == haya:
+       rotba= "مّمٌَـبـ ـࢪمـج السوࢪس"
+    elif message.from_user.id in dev:
+        rotba = "مطـور اساسي"
+    elif get.status in [ChatMemberStatus.OWNER]:
+        rotba= "المــــألك"
+    elif get.status in [ChatMemberStatus.ADMINISTRATOR]:
+        rotba= "أدمـــن"
+    else:
+        return await message.reply_text(f"يا {message.from_user.mention} انت لست مشرفا هنا")       
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR] and  dev:
+      if not message.chat.id in iddof:
+        return await message.reply_text(f"يا {message.from_user.mention}\nالايدي معفل من قبل")
+      iddof.remove(message.chat.id)
+      return await message.reply_text(f"تم فتح امر ايدي بنجاح\n\n بواسطة {rotba} ←{message.from_user.mention}")
+ 
+   
+
+
+
+@app.on_message(
+    filters.command(["ايدي","id","ا"], "")
+& filters.group
+)
+async def iddd(client, message):
+    if message.chat.id in iddof:
+      return
+    usr = await client.get_chat(message.from_user.id)
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"""🤡 ¦𝙽𝙰𝙼𝙴 :{message.from_user.mention}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{message.from_user.username}\n🎃 ¦𝙸𝙳 :{message.from_user.id}\n💌 ¦𝙱𝙸𝙾 :{usr.bio}\n✨ ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n♻️ ¦𝙸𝙳.𝙶𝚁𝙾𝚄𝙿 :{message.chat.id}""", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        ),
+    )
+
+id = {}
 @app.on_message(
      command(["قفل ايدي","تعطيل ايدي"])
      & filters.group
